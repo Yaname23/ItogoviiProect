76f1b6a48360d2ab38edd45a832391b50f241635
Yaname23 <123150404+Yaname23@users.noreply.github.com>
2023-06-20 23:05:48 +0300

    Create test_data.py

---
+++ test_data.py
@@ -0,0 +1,56 @@
+numb1 = '89001234000'
+numb2 = '+79001234000'
+numb = '+79081846488'
+numb3 = '9001234000'
+nameY = 'Новая'
+nameJ = '原千五百秋瑞'
+nameAL = 'A' #a латинская
+nameAK = 'А' #кириллицей
+num1 = '1'
+elS = '!'
+elS1 = '@'
+nameAB = 'AB' #латинские
+nameABK = 'АВ' #кириллицей
+num2 ='12'
+nameS = '!А'
+nameASK = 'А -' #кириллицей
+nameAS = 'A -' #латинские
+namePP = '  ' #два пробела
+namePY = ''
+namePOP = 'поп - корн'
+nameB30 = 'фывапролджэфывапролджэфывапролпрпоп'
+name30 = 'фывапролджэфывапролджэфывапрол'
+name2t = 'лорврромрм—лоррорм'
+nameZn = '☺☻♥♦♣♠•◘'
+nameAr = 'صسغذئآ'
+nameC = '龍門大酒家家家家'
+nameSS = '№;"?@%!*&%#'
+fam_V = 'Иван-Чаевич'
+
+
+num11 = '12345678911'
+num12 ='012346778912'
+num13 ='0123456789013'
+Lat11 ='Abcdefghijk'
+KirE = 'Абвгдеёжзик@mail.com'
+SE = '☺☻♥♦♣♠•◘♠•◘@mail.com'
+ArE = ' صسغذئآصسغذئ@mail.com '
+ChE = '龍門大酒家門大酒家酒家@gmail.com'
+Ye =  '原千五百秋瑞千五百秋瑞@gmail.com'
+SS =' №;"?@%№;"?@@gmail.com'
+NE = 'невалид@мейл.ру'
+T2T2 = 'test--2022.test@mail.com'
+DlE505 = '14jYuZsGlJjYxOYdPTJebhuJgGgWuaYBpUbxEtxKEhGtXiuBkyCIufWQqTAbaXlAQzaZOEvvxRgGaMKTFLduRaouihOuXjxHqPXqbjzkjPSCYbtFaqiDBnzNcJymvCsPTAEWlFBofUqdhmSpOihjBumquPfqWXkmEUSvsXGQAVBwZZsSXsXQYnYPrCbCGoZRoJIBOgSRJpePQWGBlPCnrIlkOdYobRLcXFgbwxRmwySAvfHLiBVyhIudSNenbvyjzZxraJzMKupefOTeKoNiIiAfiEKIejvoABMdFYcUWsibgfmcsDnExHGpozUFTetzoCTSdTGckvgJicwngwV7EKIoBeGuYLCouOGOSZWMSPpPFCVSagjiXZIayxvZVWkdomVwMjxvOxpCotcTxAsVdQSTEujFqccMxVtWQayFdFDrDOrWMsRLymSNYgVyCLuIPMlcxhzFVFhMgpRwhHYfjQVWsLpaOpkhEnkflRtqkZQgFeOe@mail.com'
+ML = 'yaname.test@gmail.com'
+
+PrL_V = 'Test2022'
+Pr7 = 'Lkjhgfd'
+pr = 'lkjhgfds'
+PrK = 'lлорпавн'
+Pr21 = 'Lkjhgfdsapoiuytrewqas'
+PrNV = 'Lkjhgfds1234'
+Pr20 = 'Lkjhgfdsapoiuytrewqa'
+
+numbN = '8900123419'
+
+
